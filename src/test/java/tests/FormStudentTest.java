5fe5ef180dff6e030b7eb82ceff2d983f81d6599
lenka <lena.malicheva@mail.ru>
2023-12-03 13:12:26 +0400

    Jenkins HW

--- src/test/java/tests/FormStudentTest.java
+++ src/test/java/tests/FormStudentTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Tag;
 import  org.junit.jupiter.api.Test;
 import pages.FormStudentPage;
 
-@Tag("Заполнение формы случайными тестовыми данными")
+
 public class FormStudentTest extends TestBase {
     FormStudentPage formStudentPage = new FormStudentPage();
     TestData testData = new TestData();
